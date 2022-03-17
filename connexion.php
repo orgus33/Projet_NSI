65d874fb915a15ffb943c923310803d5431da668
Bastien <bastien.b200405@gmail.com>
2022-03-17 22:35:55 +0100

    Base du projet

---
+++ connexion.php
@@ -0,0 +1,10 @@
+<?php
+
+include("templates/top.php")
+
+?>
+
+<?php
+  include("templates/bottom.php")
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include("templates/top.php")
+
+?>
+
+
+
+  <div id="register">
+    <a href ="connexion.php" >   
+        <button> Connexion </button> 
+      </a>
+      <a href ="inscription.php" > 
+        <button> Inscription </button>
+      </a>
+  </div>
+
+
+<?php
+  include("templates/bottom.php")
+
+?>
