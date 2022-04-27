e7009070c8f4d013aa36bc887a8b38b25d5c9d74
Bastien <bastien.b200405@gmail.com>
2022-04-27 23:17:38 +0200

    git

--- templates/top.php
+++ templates/top.php
@@ -4,6 +4,6 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" type="text/css" href="\Projet_NSI-main\styles\style.css">
+  <link rel="stylesheet" type="text/css" href="styles/">
   <title>PinTracker</title>
 </head>
