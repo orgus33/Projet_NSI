faf100103af4d12205aa5b1f1ef42c3d074dae83
Bastien <bastien.b200405@gmail.com>
2022-05-05 21:29:52 +0200

    delete folder

--- Projet_NSI-main/templates/bd.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-try {
-    $bd = new PDO("mysql:dbname=pintracker;host:localhost;charset=utf8", "root", "");
-    echo "La bdd est bien connecté";
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
